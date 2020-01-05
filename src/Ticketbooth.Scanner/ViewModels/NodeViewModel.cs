a662c61ba8183ec2cafed14ba832ef986a7a3b77
Adam Shirt <adam.shirt@developmomentum.com>
2020-01-05 14:15:54 +0000

    Scanner app node information (#16)
    * Navigation to node info
    
    * Node details view

---
+++ src/Ticketbooth.Scanner/ViewModels/NodeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Ticketbooth.Scanner.Eventing;
+using Ticketbooth.Scanner.Eventing.Args;
+using Ticketbooth.Scanner.Services.Application;
+
+namespace Ticketbooth.Scanner.ViewModels
+{
+    public class NodeViewModel : IDisposable, INotifyPropertyChanged
+    {
+        public event EventHandler<PropertyChangedEventArgs> OnPropertyChanged;
+
+        private readonly IHealthChecker _healthChecker;
+
+        public NodeViewModel(IHealthChecker healthChecker)
+        {
+            _healthChecker = healthChecker;
+            _healthChecker.OnPropertyChanged += (s, e) => OnPropertyChanged?.Invoke(s, e);
+        }
+
+        public bool IsConnected => _healthChecker.IsConnected;
+
+        public string Address => _healthChecker.NodeAddress;
+
+        public string Version => $"v{_healthChecker.NodeVersion}";
+
+        public string Network => "Cirrus Main";
+
+        public string State => _healthChecker.State;
+
+        public void Dispose()
+        {
+            _healthChecker.OnPropertyChanged -= (s, e) => OnPropertyChanged?.Invoke(s, e);
+        }
+    }
+}
